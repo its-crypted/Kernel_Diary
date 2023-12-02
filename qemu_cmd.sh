1a95dbb29b5df83846994dee2d614909762698c3
Piyush Thange <pthange19@gmail.com>
2023-12-02 23:13:22 +0530

    Adding memory & smp for the qemu
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- qemu_cmd.sh
+++ qemu_cmd.sh
@@ -2,4 +2,6 @@
 
 qemu-system-x86_64 --enable-kvm -kernel main/arch/x86/boot/bzImage \
 	-hda debian.img \
-	-append "root=/dev/sda nokaslr"
+	-append "root=/dev/sda nokaslr" \
+	-m 8g \ #Give memory of 8GB to the system
+	-smp 4 \ #Simultaneous Multiprocessor 4
