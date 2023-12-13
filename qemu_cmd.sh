945ab130b05f171ad44deee2d6489e4b69166ed1
Piyush Thange <pthange19@gmail.com>
2023-12-13 21:26:58 +0530

    Disply & serial code
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- qemu_cmd.sh
+++ qemu_cmd.sh
@@ -2,6 +2,8 @@
 
 qemu-system-x86_64 --enable-kvm -kernel main/arch/x86/boot/bzImage \
 	-hda debian.img \
-	-append "root=/dev/sda nokaslr" \
+	-append "root=/dev/sda nokaslr console=ttyS0" \
 	-m 8g \ #Give memory of 8GB to the system
 	-smp 4 \ #Simultaneous Multiprocessor 4
+	-serial stdio \ # This will output the boot output & only open loging window
+	-display none \ # This will dump all the content on the current terminal
